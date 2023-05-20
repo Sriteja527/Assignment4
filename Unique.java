32f4b543e85be2abae1c271785b2cc960c057162
Sriteja527 <79376094+Sriteja527@users.noreply.github.com>
2023-05-20 21:16:32 +0530

    Add files via upload

---
+++ Unique.java
@@ -0,0 +1,24 @@
+import java.util.*;
+ 
+public class Unique{
+public static void main(String[] args)
+{
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+ 
+    for (int i = 0; i < str.length(); i++)
+    {
+        int flag = 0;
+        for (int j = 0; j < str.length(); j++)
+        {
+            if (str.charAt(i) == str.charAt(j) && i != j)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+            System.out.print(str.charAt(i));
+    }
+}
+}
